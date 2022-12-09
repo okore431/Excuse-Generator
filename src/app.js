b8370be395b92d9c0956e5c665c6eb7bfa29baab
okore431 <olussskin@gmail.com>
2022-12-08 21:55:30 -0500

    Projrct is complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  function generateExcuse() {
+
+  let who = ['The cat','My husband','His dog','My bird'];
+  let action = ['ate','run','jumped','broke', 'hypnotized'];
+  let what = ['my nugget', 'the fish', 'his iguana'];
+  let when = ['after party','at brunch time','when I slept','during the hike','before zumba class'];
+    
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random()  * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+ 
+  let excuse = who[whoIndex] +' '+ action[actionIndex] +' '+ what[whatIndex] +' '+ when[whenIndex] +'.';
+    
+  return excuse;
+  };
+
+  document.querySelector ('#the-excuse').innerHTML = generateExcuse();
+}
